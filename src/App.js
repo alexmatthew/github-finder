c7998377dad13351f2dd05ebc96f899fb1aafb34
alexmatthew <candelarioac@gmail.com>
2019-11-19 18:39:01 -0500

    day two

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <h1>Hello From React</h1>
-      </div>
-    );
-
-  }
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+			</div>
+		);
+	}
 }
 
 export default App;
