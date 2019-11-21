53b35f3f68930ae1048b8451b8b3e03c7d7000fd
Alex <candelarioac@gmail.com>
2019-11-20 21:15:24 -0500

    second commit day 3

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const Useritem = ({ user: { login, avatar_url, html_url } }) => {
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,41 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './Useritem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      }
-    ]
-  };
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner></Spinner>;
+  } else {
     return (
       <div style={userStyle}>
-        {' '}
-        {this.state.users.map((user) => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
-        ))}{' '}
+        ))}
       </div>
     );
   }
-}
+};
 
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
