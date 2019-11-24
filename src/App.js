e57899b85b8d9fc3cd5cf13a2fa46719d1d9ccf4
Alex <candelarioac@gmail.com>
2019-11-23 21:06:10 -0500

    added alert

--- src/App.js
+++ src/App.js
@@ -2,43 +2,53 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-	state = {
-		users: [],
-		loading: false
-	};
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  };
 
-	searchUsers = async (text) => {
-		this.setState({ loading: true });
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id${process.env
-				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-		this.setState({ users: res.data.items, loading: false });
-	};
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-	//clear users from state
+  //clear users from state
 
-	clearUsers = () => this.setState({ users: [], loading: false });
-	render() {
-		const { users, loading } = this.state;
-		return (
-			<div className='App' key>
-				<Navbar />
-				<div className='container'>
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-					/>
-					<Users loading={loading} users={users} />
-				</div>
-			</div>
-		);
-	}
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alerts
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 2000);
+  };
+
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <div className='App' key>
+        <Navbar />
+        <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
